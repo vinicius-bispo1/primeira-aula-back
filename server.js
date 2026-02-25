b9ec482bddc9d0845ec1947ac50764ea20686552
vinicius-bispo1 <vini.bispo125@gmail.com>
2026-02-25 19:50:28 -0300

    🎉 feat: meu primeiro servidor

---
+++ server.js
@@ -0,0 +1,16 @@
+// Importamos o express
+const express = require("express");
+
+// criando uma aplicação express, aqui estamos inicializando o nosso servidor
+const app = express();
+const PORT = 3000;
+// "/" significa rota principal (http://localhost:3000/)
+app.get("/", (req, res) => {
+  //req = request (dados enviados pelo cliente)
+  //res = response (resposta que vamos enviar)
+  res.send("Olá!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
